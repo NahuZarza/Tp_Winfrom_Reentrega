5fd3dc08ec0f418e7a59a99a648af625a954c090
NahuZarza <nahuelalcala627@gmail.com>
2025-09-26 11:53:53 -0300

    Base del codigo

---
+++ Tp_Reentrega/Tp_WinForms.sql
@@ -0,0 +1,29 @@
+Create Database Tp_WinForms;
+Go
+Use Tp_WinForms;
+Go
+
+Create Table Categorias(
+    IdCategoria int PRIMARY KEY IDENTITY(1,1),
+    Nombre varchar(100) NOT NULL,
+)
+go
+
+Create Table Marcas(
+    IdMarca int PRIMARY KEY IDENTITY(1,1),
+    Nombre varchar(100) NOT NULL,
+)
+
+Create Table Articulos(
+    CodigoArticulo int PRIMARY KEY IDENTITY(1,1),
+    Nombre varchar(100) NOT NULL,
+    Descripcion varchar(255) NOT NULL,
+    IdCategoria int NOT NULL,
+    IdMarca int NOT NULL,
+    ImagenUrl varchar(255) NOT NULL,
+    Precio decimal(10,2) NOT NULL,
+
+    FOREIGN KEY (IdCategoria) REFERENCES Categorias(IdCategoria),
+    FOREIGN KEY (IdMarca) REFERENCES Marcas(IdMarca),
+
+)
